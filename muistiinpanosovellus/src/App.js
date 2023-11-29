e04a25679d9656fe4d9f3f05290e3c2df1366fb8
oonaidrizi <123628536+oonaidrizi@users.noreply.github.com>
2023-11-29 13:13:15 +0200

    .

--- muistiinpanosovellus/src/App.js
+++ muistiinpanosovellus/src/App.js
@@ -10,7 +10,7 @@ const App = () => {
 
   return (
     <div className="app">
-      <h1>Notes App</h1>
+      <h1>Muistiinpanosovellus</h1>
       <NoteForm addNote={addNote} />
       <NoteList notes={notes} />
     </div>
