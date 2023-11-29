1fced93e94f4f55f5cf0406133ce57f79c6f2e9c
Jouni Kiviperä <jounikivipera@gmail.com>
2023-11-29 11:50:12 +0200

    Update App.js

--- muistiinpanosovellus/src/App.js
+++ muistiinpanosovellus/src/App.js
@@ -1,8 +1,8 @@
 // App.js
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import NoteList from './NoteList';
-import NoteForm from './NoteForm';  
+import NoteList from '/components/NoteList';
+import NoteForm from './components/NoteForm';
 
 const App = () => {
   
