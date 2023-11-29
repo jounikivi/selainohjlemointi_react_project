dcad967011c75a688e0c201821984d8edcdebba0
oonaidrizi <123628536+oonaidrizi@users.noreply.github.com>
2023-11-29 11:16:49 +0200

    Update App.js

--- muistiinpanosovellus/src/App.js
+++ muistiinpanosovellus/src/App.js
@@ -1,10 +1,34 @@
+// App.js
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NoteList from './NoteList';
+import NoteForm from './NoteForm';
 
-function App() {
+const App = () => {
+  const [notes, setNotes] = useState([]);
+
+  const addNote = (newNote) => {
+    setNotes([...notes, newNote]);
+  };
+
+  const deleteNote = (noteId) => {
+    const updatedNotes = notes.filter((note) => note.id !== noteId);
+    setNotes(updatedNotes);
+  };
+
   return (
-    <div className="App">
-      <h1>Loren Ipmsum</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <NoteList notes={notes} deleteNote={deleteNote} />
+        </Route>
+        <Route path="/add">
+          <NoteForm addNote={addNote} />
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
+
