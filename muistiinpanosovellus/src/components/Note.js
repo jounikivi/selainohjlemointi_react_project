b6a0f32ea70ba74303dd1e58a22b39be76a59191
Jouni Kiviperä <jounikivipera@gmail.com>
2023-12-04 08:58:45 +0200

    lisätty muokkaa nappi

--- muistiinpanosovellus/src/components/Note.js
+++ muistiinpanosovellus/src/components/Note.js
@@ -1,12 +1,13 @@
 // Note.js
 import React from 'react';
 
-const Note = ({ note, deleteNote }) => {
+const Note = ({ note, deleteNote, editNote }) => {
   return (
     <div>
       <p>{note.text}</p>
       <p>Kategoria: {note.category}</p>
       <button onClick={() => deleteNote(note.id)}>Poista muistiinpano</button>
+      <button onClick={() => editNote(note.id)}>Muokkaa</button>
     </div>
   );
 };
