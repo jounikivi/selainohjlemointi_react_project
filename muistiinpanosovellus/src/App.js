bdc4519d2da6752f534700b83c39e2d5e9aad54e
Jouni Kiviperä <jounikivipera@gmail.com>
2023-12-10 15:57:39 +0200

    muokkaa nappi toimii

--- muistiinpanosovellus/src/App.js
+++ muistiinpanosovellus/src/App.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import NoteList from './components/NoteList';
 import NoteForm from './components/NoteForm';
+import Note from './components/Note';
 import './App.css';
 
 const App = () => {
   const [notes, setNotes] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedNote, setEditedNote] = useState({ id: '', text: '', category: '' });
 
   const addNote = (newNote) => {
     setNotes([...notes, newNote]);
@@ -15,15 +18,71 @@ const App = () => {
     setNotes(updatedNotes);
   };
 
+  const editNote = (id, editedText, editedCategory) => {
+    const updatedNotes = notes.map((note) => {
+      if (note.id === id) {
+        return {
+          ...note,
+          text: editedText,
+          category: editedCategory,
+        };
+      }
+      return note;
+    });
+    setNotes(updatedNotes);
+    setIsEditing(false);
+  };
+
+  const startEditing = (note) => {
+    setIsEditing(true);
+    setEditedNote({
+      id: note.id,
+      text: note.text,
+      category: note.category,
+    });
+  };
+
+  const cancelEditing = () => {
+    setIsEditing(false);
+    setEditedNote({ id: '', text: '', category: '' });
+  };
+
   return (
     <div className="container">
       <h1>Muistiinpanosovellus</h1>
-      <div className='form-container'>
+      <div className="form-container">
         <NoteForm addNote={addNote} />
       </div>
       {notes.length > 0 && (
-        <div className='note-list'>
-          <NoteList notes={notes} deleteNote={deleteNote} />
+        <div className="note-list">
+          {notes.map((note) => (
+            <div key={note.id} className="note">
+              {isEditing && editedNote.id === note.id ? (
+                <div>
+                  <input
+                    type="text"
+                    value={editedNote.text}
+                    onChange={(e) => setEditedNote({ ...editedNote, text: e.target.value })}
+                  />
+                  <input
+                    type="text"
+                    value={editedNote.category}
+                    onChange={(e) => setEditedNote({ ...editedNote, category: e.target.value })}
+                  />
+                  <button onClick={() => editNote(editedNote.id, editedNote.text, editedNote.category)}>
+                    Tallenna muutokset
+                  </button>
+                  <button onClick={cancelEditing}>Peruuta</button>
+                </div>
+              ) : (
+                <Note
+                  note={note}
+                  deleteNote={deleteNote}
+                  editNote={startEditing}
+                />
+              )}
+            </div>
+          ))}
         </div>
       )}
     </div>
--- muistiinpanosovellus/src/components/Note.js
+++ muistiinpanosovellus/src/components/Note.js
@@ -1,13 +1,40 @@
 // Note.js
-import React from 'react';
+import React, { useState } from 'react';
 
 const Note = ({ note, deleteNote, editNote }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedText, setEditedText] = useState(note.text);
+  const [editedCategory, setEditedCategory] = useState(note.category);
+
+  const handleEdit = () => {
+    editNote(note.id, editedText, editedCategory);
+    setIsEditing(false);
+  };
+
   return (
     <div>
-      <p>{note.text}</p>
-      <p>Kategoria: {note.category}</p>
-      <button onClick={() => deleteNote(note.id)}>Poista muistiinpano</button>
-      <button onClick={() => editNote(note.id)}>Muokkaa</button>
+      {isEditing ? (
+        <div>
+          <input
+            type="text"
+            value={editedText}
+            onChange={(e) => setEditedText(e.target.value)}
+          />
+          <input
+            type="text"
+            value={editedCategory}
+            onChange={(e) => setEditedCategory(e.target.value)}
+          />
+          <button onClick={handleEdit}>Tallenna muutokset</button>
+        </div>
+      ) : (
+        <div>
+          <p>{note.text}</p>
+          <p>Kategoria: {note.category}</p>
+          <button onClick={() => deleteNote(note.id)}>Poista muistiinpano</button>
+          <button onClick={() => setIsEditing(true)}>Muokkaa</button>
+        </div>
+      )}
     </div>
   );
 };
