946305f08e22fb135710a631476106f0bc375695
Jouni Kiviperä <jounikivipera@gmail.com>
2023-12-11 08:54:23 +0200

    lisätty kommenteja

--- muistiinpanosovellus/src/App.js
+++ muistiinpanosovellus/src/App.js
@@ -1,23 +1,31 @@
-import React, { useState } from 'react';
-import NoteList from './components/NoteList';
-import NoteForm from './components/NoteForm';
-import Note from './components/Note';
-import './App.css';
+import React, { useState } from "react";
+import NoteList from "./components/NoteList";
+import NoteForm from "./components/NoteForm";
+import Note from "./components/Note";
+import "./App.css";
 
 const App = () => {
+  // Tilamuuttujat
   const [notes, setNotes] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
-  const [editedNote, setEditedNote] = useState({ id: '', text: '', category: '' });
+  const [editedNote, setEditedNote] = useState({
+    id: "",
+    text: "",
+    category: "",
+  });
 
+  // Lisää uusi muistiinpano
   const addNote = (newNote) => {
     setNotes([...notes, newNote]);
   };
 
+  // Poistaa muistiinpanon
   const deleteNote = (id) => {
     const updatedNotes = notes.filter((note) => note.id !== id);
     setNotes(updatedNotes);
   };
 
+  // Muokkaa muistiinpanoa
   const editNote = (id, editedText, editedCategory) => {
     const updatedNotes = notes.map((note) => {
       if (note.id === id) {
@@ -33,6 +41,7 @@ const App = () => {
     setIsEditing(false);
   };
 
+  // Asettaa muokkaustilan päälle
   const startEditing = (note) => {
     setIsEditing(true);
     setEditedNote({
@@ -42,39 +51,56 @@ const App = () => {
     });
   };
 
+  // Peruuttaa muokkauksen
   const cancelEditing = () => {
     setIsEditing(false);
-    setEditedNote({ id: '', text: '', category: '' });
+    setEditedNote({ id: "", text: "", category: "" });
   };
 
   return (
     <div className="container">
       <h1>Muistiinpanosovellus</h1>
       <div className="form-container">
+        {/* Lisää uuden muistiinpanon lomake */}
         <NoteForm addNote={addNote} />
       </div>
+      {/* Näyttää muistiinpanot, jos niitä on */}
       {notes.length > 0 && (
         <div className="note-list">
           {notes.map((note) => (
             <div key={note.id} className="note">
               {isEditing && editedNote.id === note.id ? (
                 <div>
+                  {/* Muokkausnäkymä, jos muokkaustila on päällä */}
                   <input
                     type="text"
                     value={editedNote.text}
-                    onChange={(e) => setEditedNote({ ...editedNote, text: e.target.value })}
+                    onChange={(e) =>
+                      setEditedNote({ ...editedNote, text: e.target.value })
+                    }
                   />
                   <input
                     type="text"
                     value={editedNote.category}
-                    onChange={(e) => setEditedNote({ ...editedNote, category: e.target.value })}
+                    onChange={(e) =>
+                      setEditedNote({ ...editedNote, category: e.target.value })
+                    }
                   />
-                  <button onClick={() => editNote(editedNote.id, editedNote.text, editedNote.category)}>
+                  <button
+                    onClick={() =>
+                      editNote(
+                        editedNote.id,
+                        editedNote.text,
+                        editedNote.category
+                      )
+                    }
+                  >
                     Tallenna muutokset
                   </button>
                   <button onClick={cancelEditing}>Peruuta</button>
                 </div>
               ) : (
+                // Näyttää muistiinpanon, jos muokkaustila ei ole päällä
                 <Note
                   note={note}
                   deleteNote={deleteNote}
--- muistiinpanosovellus/src/components/NoteForm.js
+++ muistiinpanosovellus/src/components/NoteForm.js
@@ -1,45 +1,53 @@
-// NoteForm.js
-import React, { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const NoteForm = ({ addNote }) => {
-  const [noteTitle, setNoteTitle] = useState('');
-  const [noteText, setNoteText] = useState('');
-  const [category, setCategory] = useState('');
+  // Tilamuuttujat
+  const [noteTitle, setNoteTitle] = useState("");
+  const [noteText, setNoteText] = useState("");
+  const [category, setCategory] = useState("");
 
+  // Käsittele lomakkeen lähettäminen
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (noteText.trim() === '' || category.trim() === '') return;
+    // Tarkistetaan, etteivät tekstikentät ole tyhjiä
+    if (noteText.trim() === "" || category.trim() === "") return;
+
+    // Lisää uusi muistiinpano ja nollaa kentät
     addNote({
       id: uuidv4(),
       title: noteTitle,
       text: noteText,
-      category
+      category,
     });
-    setNoteTitle('');
-    setNoteText('');
-    setCategory('');
+    setNoteTitle("");
+    setNoteText("");
+    setCategory("");
   };
 
   return (
     <form onSubmit={handleSubmit}>
+      {/* Otsikkokenttä */}
       <input
         type="text"
         placeholder="Otsikko"
         value={noteTitle}
         onChange={(e) => setNoteTitle(e.target.value)}
       />
+      {/* Tekstikenttä muistiinpanon kirjoittamiseen */}
       <textarea
         placeholder="Lisää muistiinpano..."
         value={noteText}
         onChange={(e) => setNoteText(e.target.value)}
       ></textarea>
+      {/* Kategorian syöttökenttä */}
       <input
         type="text"
         placeholder="Kategoria..."
         value={category}
         onChange={(e) => setCategory(e.target.value)}
       />
+      {/* Lähetä-nappi */}
       <button type="submit">Lisää muistiinpano</button>
     </form>
   );
